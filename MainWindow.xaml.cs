b6031d4e2ddb0819c3d185bce23068c09e133f51
satbir1992 <satbirsingh.singh4@gmail.com>
2023-02-07 22:40:45 +0800

    Coding has been started and Data has been displayed in the List View from Linked List

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Galileo6;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Galileo6;
+//using Galileo6;
 
 namespace Marlin_Space_System_App
 {
@@ -25,5 +26,43 @@ namespace Marlin_Space_System_App
         {
             InitializeComponent();
         }
+        LinkedList<double> SensorAList = new LinkedList<double>();
+        LinkedList<double> SensorBList= new LinkedList<double>();
+        private void LoadDataList()
+        {
+            SensorAList.Clear();
+            SensorBList.Clear();
+            int ListSize = 400;
+            ReadData readSensorData = new ReadData();
+            for(int i = 0; i< ListSize; i++)
+            {
+                SensorAList.AddLast(readSensorData.SensorA(MuValue.Value, SigmaValue.Value));
+                SensorBList.AddLast(readSensorData.SensorB(MuValue.Value, SigmaValue.Value));
+            }
+        }
+        private void ShowAllSensorData()
+        {
+            int ListSize = 400;
+            ListViewDisplay.Items.Clear();
+            for(int i=0; i< ListSize; i++)
+            {
+                ListViewDisplay.Items.Add(new
+                {
+                    SensorAList = SensorAList.ElementAt(i).ToString(),
+                    SensorBList = SensorBList.ElementAt(i).ToString()
+                });
+            }
+            
+
+        }
+
+        private void LoadData_Click(object sender, RoutedEventArgs e)
+        {
+            LoadDataList();
+            ShowAllSensorData();
+        }
     }
+    
+
 }
+
