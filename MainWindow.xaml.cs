2dadbc0aaea6dac47fc6b34815858b58669a26e2
satbir1992 <satbirsingh.singh4@gmail.com>
2023-02-07 17:10:20 +0800

    Galileo6 DLL has been added to the project and some proper name has been given to the ListBox and List View

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Xceed.Wpf.Samples.SampleData;
+using Galileo6;
 
 namespace Marlin_Space_System_App
 {
